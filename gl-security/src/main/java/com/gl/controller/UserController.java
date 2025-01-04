402da2ad19a803aa643058f6da4330ffe91c825a
glancake <909914088@qq.com>
2025-01-04 18:30:50 +0800

    a

--- gl-security/src/main/java/com/gl/controller/UserController.java
+++ gl-security/src/main/java/com/gl/controller/UserController.java
@@ -3,6 +3,7 @@ package com.gl.controller;
 import com.gl.api.CommonResult;
 import com.gl.domain.GlAccount;
 import com.gl.service.GlAccountService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,13 +21,18 @@ public class UserController {
      * 更新头像
      */
     @PostMapping("/update_avatar")
+    @Operation(summary = "更新头像")
     public CommonResult updateAvatar(@RequestParam("user_id") Long userId,
                                      @RequestBody MultipartFile avatar) throws  IOException {
-        boolean result = glAccountService.updateAvatar(userId, avatar);
+        glAccountService.updateAvatar(userId, avatar);
         return CommonResult.success(null, "update avatar successfully");
     }
 
+    /**
+     * 获取用户信息
+     */
     @GetMapping("/user_info")
+    @Operation(summary = "获取用户信息")
     public CommonResult getUserInfo() throws IOException {
         GlAccount user = glAccountService.getUserInfo();
         return CommonResult.success(user);
